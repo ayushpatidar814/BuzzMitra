55a146fc917ceb4e4389e3152b1ed1cdca58d992
ayushpatidar814 <ayushpatidar814@gmail.com>
2025-08-05 18:52:40 +0530

    email sent added

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,18 @@
+import express from 'express'
+import { acceptConnectionsRequest, discoverUsers, followUser, getUserConnections, getUserData, sendConnectionRequest, unfollowUser, updatedUserData } from '../controllers/userController.js'
+import { protect } from '../middlewares/auth.js';
+import { upload } from '../configs/multer.js';
+
+const userRouter = express.Router();
+
+userRouter.get('/data', protect, getUserData)
+userRouter.post('/update', upload.fields([{name: 'profile', maxCount:1}, {name: 'cover', maxCount: 1}]), protect, updatedUserData)
+userRouter.post('/discover', protect, discoverUsers)
+userRouter.post('/follow', protect, followUser)
+userRouter.post('/unfollow', protect, unfollowUser)
+userRouter.post('/connect', protect, sendConnectionRequest)
+userRouter.post('/accept', protect, acceptConnectionsRequest)
+userRouter.get('/connections', protect, getUserConnections)
+
+
+export default userRouter
