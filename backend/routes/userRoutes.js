b2fb6b3e4b31deb571c8c7b6e41440bd9e633bc9
ayushpatidar814 <ayushpatidar814@gmail.com>
2025-08-06 15:44:50 +0530

    backend complete

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,7 +1,8 @@
 import express from 'express'
-import { acceptConnectionsRequest, discoverUsers, followUser, getUserConnections, getUserData, sendConnectionRequest, unfollowUser, updatedUserData } from '../controllers/userController.js'
+import { acceptConnectionsRequest, discoverUsers, followUser, getUserConnections, getUserData, getUserProfiles, sendConnectionRequest, unfollowUser, updatedUserData } from '../controllers/userController.js'
 import { protect } from '../middlewares/auth.js';
 import { upload } from '../configs/multer.js';
+import { getUserRecentMessages } from '../controllers/messageController.js';
 
 const userRouter = express.Router();
 
@@ -13,6 +14,8 @@ userRouter.post('/unfollow', protect, unfollowUser)
 userRouter.post('/connect', protect, sendConnectionRequest)
 userRouter.post('/accept', protect, acceptConnectionsRequest)
 userRouter.get('/connections', protect, getUserConnections)
+userRouter.post('/profiles', protect, getUserProfiles)
+userRouter.get('/recent-messages', protect, getUserRecentMessages)
 
 
 export default userRouter
--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,9 @@ import {inngest, functions} from './inngest/index.js';
 import { serve } from 'inngest/express';
 import { clerkMiddleware } from '@clerk/express'
 import userRouter from './routes/userRoutes.js';
+import postRouter from './routes/postRoutes.js';
+import storyRouter from './routes/storyRoutes.js';
+import messageRouter from './routes/messageRoutes.js';
 
 const app = express();
 
@@ -16,13 +19,16 @@ app.use(cors());
 app.use(clerkMiddleware());
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send('Hello World Ayush!');
 });
 
 app.use('/api/inngest', serve({ client: inngest, functions }));
 app.use('/api/user', userRouter)
+app.use('/api/post', postRouter)
+app.use('/api/story', storyRouter)
+app.use('/api/message', messageRouter)
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
